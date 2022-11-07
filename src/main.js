994444f71d31538dc028243efba4722c410ab0d0
Obcido <jonathan.harkonen@gmail.com>
2022-11-08 00:42:03 +0200

    add dependencies and stuff

--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import VueCookies from 'vue3-cookies'
 
-createApp(App).mount('#app')
+createApp(App).use(VueCookies, {
+    expireTimes: '1d',
+    path: '/',
+    domain: '',
+    secure: true,
+    sameSite: 'Strict'
+}).mount('#app')
